bd0ee099e30585465c51996e997b993b0a796706
deadlydog <deadlydog@hotmail.com>
2025-11-30 23:47:01 -0600

    test: Update logic and add tests for avoiding hitting snakes

--- src/Battlesnake.Domain.Tests/MovementStrategies/AvoidHittingSnakesStrategyTests.cs
+++ src/Battlesnake.Domain.Tests/MovementStrategies/AvoidHittingSnakesStrategyTests.cs
@@ -11,6 +11,9 @@ public class AvoidHittingSnakesStrategyTests
 	{
 		AvoidHittingSnakesStrategy.SnakeBodyPenaltyScore.ShouldBeLessThan(0);
 		AvoidHittingSnakesStrategy.SnakeTailPenaltyScore.ShouldBeLessThan(0);
+
+		// Score for hitting body should be more severe than hitting tail.
+		AvoidHittingSnakesStrategy.SnakeBodyPenaltyScore.ShouldBeLessThan(AvoidHittingSnakesStrategy.SnakeTailPenaltyScore);
 	}
 
 	[Fact]
@@ -61,5 +64,79 @@ public class AvoidHittingSnakesStrategyTests
 		directionScores.Down.ShouldBe(AvoidHittingSnakesStrategy.SnakeBodyPenaltyScore); // Reduced score towards opponent's body
 		directionScores.Left.ShouldBe(0);
 		directionScores.Right.ShouldBe(0);
+	}
+
+	[Fact]
+	public void WhenHeadIsSurroundedByOtherSnakeExceptToTheRight_ThenScoresForAllDirectionsExceptRightAreReduced()
+	{
+		// Arrange
+		var board = new Board(11, 11);
+		board.AddSnake("player", 100, new List<Coordinate>
+		{
+			new Coordinate(5, 5), // Head
+			new Coordinate(5, 6), // Own body is directly above head
+			new Coordinate(5, 7)
+		}, true);
+		board.AddSnake("opponent", 100, new List<Coordinate>
+		{
+			new Coordinate(4, 4), // Opponent's body is directly below head
+			new Coordinate(5, 4),
+			new Coordinate(6, 4)
+		}, false);
+		board.AddSnake("opponent2", 100, new List<Coordinate>
+		{
+			new Coordinate(4, 5), // Opponent's head is directly to the left of head
+			new Coordinate(4, 6),
+			new Coordinate(4, 7)
+		}, false);
+
+		// Act
+		var directionScores = AvoidHittingSnakesStrategy.CalculateDirectionScores(board);
+
+		// Assert
+		directionScores.Up.ShouldBe(AvoidHittingSnakesStrategy.SnakeBodyPenaltyScore);
+		directionScores.Down.ShouldBe(AvoidHittingSnakesStrategy.SnakeBodyPenaltyScore);
+		directionScores.Left.ShouldBe(AvoidHittingSnakesStrategy.SnakeBodyPenaltyScore);
+		directionScores.Right.ShouldBe(0);
+	}
+
+	[Fact]
+	public void WhenHeadIsSurroundedBySnakesAndOneIsATail_ItShouldGiveTheTailDirectionTheHighestRanking()
+	{
+		// Arrange
+		var board = new Board(11, 11);
+		board.AddSnake("player", 100, new List<Coordinate>
+		{
+			new Coordinate(5, 5), // Head
+			new Coordinate(5, 6), // Own body is directly above head
+			new Coordinate(5, 7)
+		}, true);
+		board.AddSnake("opponent", 100, new List<Coordinate>
+		{
+			new Coordinate(4, 4), // Opponent's body is directly below head
+			new Coordinate(5, 4),
+			new Coordinate(6, 4)
+		}, false);
+		board.AddSnake("opponent2", 100, new List<Coordinate>
+		{
+			new Coordinate(4, 5), // Opponent's head is directly to the left of head
+			new Coordinate(4, 6),
+			new Coordinate(4, 7)
+		}, false);
+		board.AddSnake("opponent3", 100, new List<Coordinate>
+		{
+			new Coordinate(8, 5),
+			new Coordinate(7, 5),
+			new Coordinate(6, 5) // Opponent's tail is directly to the right of head
+		}, false);
+
+		// Act
+		var directionScores = AvoidHittingSnakesStrategy.CalculateDirectionScores(board);
+
+		// Assert
+		directionScores.Up.ShouldBe(AvoidHittingSnakesStrategy.SnakeBodyPenaltyScore);
+		directionScores.Down.ShouldBe(AvoidHittingSnakesStrategy.SnakeBodyPenaltyScore);
+		directionScores.Left.ShouldBe(AvoidHittingSnakesStrategy.SnakeBodyPenaltyScore);
+		directionScores.Right.ShouldBe(AvoidHittingSnakesStrategy.SnakeTailPenaltyScore); // Less severe penalty for tail
 	}
 }
--- src/Battlesnake.Domain/MovementStrategies/AvoidHittingSnakesStrategy.cs
+++ src/Battlesnake.Domain/MovementStrategies/AvoidHittingSnakesStrategy.cs
@@ -5,7 +5,7 @@ namespace Battlesnake.Domain.MovementStrategies;
 public class AvoidHittingSnakesStrategy : IMovementStrategy
 {
 	public static readonly int SnakeBodyPenaltyScore = -500;
-	public static readonly int SnakeTailPenaltyScore = -100;
+	public static readonly int SnakeTailPenaltyScore = -100; // Lower penalty for tails since they will likely move, unless the snake eats food on next turn.
 
 	public static DirectionScores CalculateDirectionScores(Board board)
 	{
@@ -18,6 +18,17 @@ public class AvoidHittingSnakesStrategy : IMovementStrategy
 			var cellAbove = board.GetCell(playerHead.X, playerHead.Y + 1);
 			switch (cellAbove.Content)
 			{
+				case BoardCellContent.SnakeHead:
+					// If the other snake is only 1 cell long, it's probably likely it will be gone next turn, unless it eats food.
+					if (cellAbove.OccupyingSnake != null && cellAbove.OccupyingSnake.Length > 1)
+					{
+						directionScores.AddScore(MoveDirections.Up, SnakeBodyPenaltyScore);
+					}
+					else
+					{
+						directionScores.AddScore(MoveDirections.Up, SnakeTailPenaltyScore);
+					}
+					break;
 				case BoardCellContent.SnakeBody: directionScores.AddScore(MoveDirections.Up, SnakeBodyPenaltyScore); break;
 				case BoardCellContent.SnakeTail: directionScores.AddScore(MoveDirections.Up, SnakeTailPenaltyScore); break;
 			}
@@ -27,6 +38,17 @@ public class AvoidHittingSnakesStrategy : IMovementStrategy
 			var cellBelow = board.GetCell(playerHead.X, playerHead.Y - 1);
 			switch (cellBelow.Content)
 			{
+				case BoardCellContent.SnakeHead:
+					// If the other snake is only 1 cell long, it's probably likely it will be gone next turn, unless it eats food.
+					if (cellBelow.OccupyingSnake != null && cellBelow.OccupyingSnake.Length > 1)
+					{
+						directionScores.AddScore(MoveDirections.Down, SnakeBodyPenaltyScore);
+					}
+					else
+					{
+						directionScores.AddScore(MoveDirections.Down, SnakeTailPenaltyScore);
+					}
+					break;
 				case BoardCellContent.SnakeBody: directionScores.AddScore(MoveDirections.Down, SnakeBodyPenaltyScore); break;
 				case BoardCellContent.SnakeTail: directionScores.AddScore(MoveDirections.Down, SnakeTailPenaltyScore); break;
 			}
@@ -36,6 +58,17 @@ public class AvoidHittingSnakesStrategy : IMovementStrategy
 			var cellLeft = board.GetCell(playerHead.X - 1, playerHead.Y);
 			switch (cellLeft.Content)
 			{
+				case BoardCellContent.SnakeHead:
+					// If the other snake is only 1 cell long, it's probably likely it will be gone next turn, unless it eats food.
+					if (cellLeft.OccupyingSnake != null && cellLeft.OccupyingSnake.Length > 1)
+					{
+						directionScores.AddScore(MoveDirections.Left, SnakeBodyPenaltyScore);
+					}
+					else
+					{
+						directionScores.AddScore(MoveDirections.Left, SnakeTailPenaltyScore);
+					}
+					break;
 				case BoardCellContent.SnakeBody: directionScores.AddScore(MoveDirections.Left, SnakeBodyPenaltyScore); break;
 				case BoardCellContent.SnakeTail: directionScores.AddScore(MoveDirections.Left, SnakeTailPenaltyScore); break;
 			}
@@ -45,6 +78,17 @@ public class AvoidHittingSnakesStrategy : IMovementStrategy
 			var cellRight = board.GetCell(playerHead.X + 1, playerHead.Y);
 			switch (cellRight.Content)
 			{
+				case BoardCellContent.SnakeHead:
+					// If the other snake is only 1 cell long, it's probably likely it will be gone next turn, unless it eats food.
+					if (cellRight.OccupyingSnake != null && cellRight.OccupyingSnake.Length > 1)
+					{
+						directionScores.AddScore(MoveDirections.Right, SnakeBodyPenaltyScore);
+					}
+					else
+					{
+						directionScores.AddScore(MoveDirections.Right, SnakeTailPenaltyScore);
+					}
+					break;
 				case BoardCellContent.SnakeBody: directionScores.AddScore(MoveDirections.Right, SnakeBodyPenaltyScore); break;
 				case BoardCellContent.SnakeTail: directionScores.AddScore(MoveDirections.Right, SnakeTailPenaltyScore); break;
 			}
